e4fa6facc695fa0340fdf89180801b2772bfb678
gbd850 <mmyszaki@gmail.com>
2024-02-11 20:22:23 +0100

    Edit create book page

--- src/Form/BookFormType.php
+++ src/Form/BookFormType.php
@@ -4,6 +4,9 @@ namespace App\Form;
 
 use App\Entity\Book;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,11 +15,41 @@ class BookFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('author')
-            ->add('releaseYear')
-            ->add('rating')
-            ->add('quantity')
+            ->add('title', TextType::class, [
+                'label' => false,
+                'attr' => [
+                    'placeholder' => 'Title',
+                    'class' => 'text-lg border rounded p-2 m-2 min-w-full'
+                ]
+            ])
+            ->add('author', TextType::class, [
+                'label' => false,
+                'attr' => [
+                    'placeholder' => 'Author',
+                    'class' => 'text-lg border rounded p-2 m-2 min-w-full'
+                ]
+            ])
+            ->add('releaseYear', IntegerType::class, [
+                'label' => false,
+                'attr' => [
+                    'placeholder' => 'Release year',
+                    'class' => 'text-lg border rounded p-2 m-2 min-w-full'
+                ]
+            ])
+            ->add('rating', NumberType::class, [
+                'label' => false,
+                'attr' => [
+                    'placeholder' => 'Rating',
+                    'class' => 'text-lg border rounded p-2 m-2 min-w-full'
+                ]
+            ])
+            ->add('quantity', IntegerType::class, [
+                'label' => false,
+                'attr' => [
+                    'placeholder' => 'Quantity',
+                    'class' => 'text-lg border rounded p-2 m-2 min-w-full'
+                ]
+            ])
         ;
     }
 
